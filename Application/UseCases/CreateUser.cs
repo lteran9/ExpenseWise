42eaa39aeae94acf56aa9d0471bd55ab88b579b8
LT <support@teran.tech>
2025-02-23 08:41:02 -0700

    Added FluentValidation NuGet packages. Created first use case for creating user accounts.

---
+++ Application/UseCases/CreateUser.cs
@@ -0,0 +1,76 @@
+using System;
+using Application.UseCases.FluentValidation;
+using Application.UseCases.MediatR;
+using Application.UseCases.Ports;
+using Core.Entities;
+using FluentValidation;
+using MediatR;
+
+namespace Application.UseCases
+{
+   public class CreateUser : BaseRequestHandler<CreateUserRequest, CreateUserResponse>
+   {
+      private readonly ISqlDatabase<User> _repository;
+      private readonly AbstractValidator<CreateUserRequest> _validator;
+
+      public CreateUser(ISqlDatabase<User> repository)
+      {
+         _repository = repository;
+         _validator = new CreateUserRequestValidator();
+      }
+
+      public override async Task<ResponseWrapper<CreateUserResponse>> Handler(CreateUserRequest request, CancellationToken cancellationToken)
+      {
+         var validationResult = await _validator.ValidateAsync(request);
+         if (validationResult.IsValid)
+         {
+            var user =
+               new User()
+               {
+                  Name = request.Name,
+                  Email = request.Email,
+                  Phone = request.Phone
+               };
+
+            var response = await _repository.Create(user);
+            if (response != null)
+            {
+               return Successful(
+                  new CreateUserResponse()
+                  {
+                     Id = response.Id,
+                     UniqueKey = response.UniqueKey
+                  });
+            }
+            else
+            {
+               return Failed(default);
+            }
+         }
+
+         return Invalid(validationResult.Errors.Select(x => x.ErrorMessage).ToList());
+      }
+   }
+
+   public class CreateUserRequest : IRequest<ResponseWrapper<CreateUserResponse>>
+   {
+      public string Name { get; set; }
+      public string Email { get; set; }
+      public string Phone { get; set; }
+      public string Password { get; set; }
+
+      public CreateUserRequest()
+      {
+         Name = string.Empty;
+         Email = string.Empty;
+         Phone = string.Empty;
+         Password = string.Empty;
+      }
+   }
+
+   public class CreateUserResponse
+   {
+      public int Id { get; set; }
+      public Guid UniqueKey { get; set; }
+   }
+}
---
+++ Application/UseCases/FluentValidation/CreateUserRequestValidator.cs
@@ -0,0 +1,21 @@
+using System;
+using FluentValidation;
+
+namespace Application.UseCases.FluentValidation
+{
+   internal class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+   {
+      public CreateUserRequestValidator()
+      {
+         RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Please provide a name.");
+         RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Please provide an email address.");
+         RuleFor(x => x.Phone)
+            .NotEmpty()
+            .WithMessage("Please provide a phone number for the account.");
+      }
+   }
+}
