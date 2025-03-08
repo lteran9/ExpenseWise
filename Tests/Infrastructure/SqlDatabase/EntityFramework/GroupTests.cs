686bd21ca4987085a03d26216b750f10539d113e
LT <support@teran.tech>
2025-03-07 19:09:04 -0700

    Added GroupTests case file for db records.

---
+++ Tests/Infrastructure/SqlDatabase/EntityFramework/GroupTests.cs
@@ -0,0 +1,32 @@
+using System;
+using ExpenseWise.Infrastructure.Sql;
+using ExpenseWise.SqlDatabase;
+
+namespace Tests.Infrastructure
+{
+   public class GroupTests
+   {
+      [Fact]
+      public void Create()
+      {
+         using (var context = new CoreContext())
+         {
+            var group =
+               new Group()
+               {
+                  Name = "SampleGroup",
+                  OwnerId = 1,
+                  UniqueKey = Guid.NewGuid(),
+                  CreatedAt = DateTime.Now,
+                  UpdatedAt = DateTime.Now,
+               };
+
+            var dbGroup = context.Add(group);
+            var result = context.SaveChanges();
+
+            Assert.True(dbGroup.Entity.Id > 0);
+            Assert.True(result > 0);
+         }
+      }
+   }
+}
