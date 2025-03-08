61f8d55c21d77a31daaba8596ce1e67213035b19
LT <support@teran.tech>
2025-03-07 19:12:41 -0700

    Added MemberOfTests case file for db records.

---
+++ Tests/Infrastructure/SqlDatabase/EntityFramework/MemberOfTests.cs
@@ -0,0 +1,51 @@
+using System;
+using ExpenseWise.Infrastructure.Sql;
+using ExpenseWise.SqlDatabase;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.Infrastructure
+{
+   public class MemberOfTests
+   {
+      [Fact]
+      public void Create()
+      {
+         using (var context = new CoreContext())
+         {
+            var membership
+               = new MemberOf()
+               {
+                  UserId = 1,
+                  GroupId = 1,
+                  CreatedAt = DateTime.Now,
+                  UpdatedAt = DateTime.Now
+               };
+
+            var dbMembership = context.Add(membership);
+            var result = context.SaveChanges();
+
+            Assert.True(dbMembership.Entity.Id > 0);
+            Assert.True(result > 0);
+         }
+      }
+
+      [Fact]
+      public void Create_Duplicate()
+      {
+         using (var context = new CoreContext())
+         {
+            var membership
+               = new MemberOf()
+               {
+                  UserId = 1,
+                  GroupId = 1,
+                  CreatedAt = DateTime.Now,
+                  UpdatedAt = DateTime.Now
+               };
+
+            context.Add(membership);
+            Assert.Throws<DbUpdateException>(() => context.SaveChanges());
+         }
+      }
+   }
+}
