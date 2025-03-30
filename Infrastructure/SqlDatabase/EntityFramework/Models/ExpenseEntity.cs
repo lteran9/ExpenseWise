9effbf1f17fb1f7ad672dccea55a259bc7f0c8ca
LT <support@teran.tech>
2025-03-30 08:17:00 -0700

    Created remaining entities (Expense & Split).

---
+++ Infrastructure/SqlDatabase/EntityFramework/Models/ExpenseEntity.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.SqlDatabase
+{
+   [Table("expenses")]
+   public class ExpenseEntity
+   {
+      [Key, Column("id")]
+      public int Id { get; set; }
+
+      [Column("settled")]
+      public bool Settled { get; set; }
+
+      [Column("description")]
+      public string Description { get; set; }
+      [Column("currency")]
+      public string Currency { get; set; }
+
+      [Column("amount")]
+      public decimal Amount { get; set; }
+
+      [Column("unique_key")]
+      public Guid UniqueKey { get; set; }
+
+      [Column("created_at")]
+      public DateTime CreatedAt { get; set; }
+      [Column("updated_at")]
+      public DateTime UpdatedAt { get; set; }
+
+      public ExpenseEntity()
+      {
+         Description = string.Empty;
+         Currency = string.Empty;
+      }
+   }
+}
